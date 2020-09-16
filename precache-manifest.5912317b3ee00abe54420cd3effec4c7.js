b00f0c8541f7110bc477ab3c41e40f4a6be6847b
johnjohnjackson11 <johnjohnjackson11@gmail.com>
2020-09-16 17:18:29 -0500

    Updates

---
+++ precache-manifest.5912317b3ee00abe54420cd3effec4c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051eff1a1a0615d47d004e898e37f56b",
+    "url": "/gh-pages-learn-menu/index.html"
+  },
+  {
+    "revision": "b0d031ef79e0444130e1",
+    "url": "/gh-pages-learn-menu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e1e0f867d3026accc48",
+    "url": "/gh-pages-learn-menu/static/js/2.2bea1654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-learn-menu/static/js/2.2bea1654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d031ef79e0444130e1",
+    "url": "/gh-pages-learn-menu/static/js/main.309ada4b.chunk.js"
+  },
+  {
+    "revision": "601f9e93a2705c428ad9",
+    "url": "/gh-pages-learn-menu/static/js/runtime-main.3d71770e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-learn-menu/static/media/logo.5d5d9eef.svg"
+  }
+]);
